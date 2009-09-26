771307a3b7c0574e771bf3510db108f604a3a875
guillobuteler <guillobuteler@08861704-a7f1-11de-892d-7b2b76e7707e>
2009-09-26 04:17:00 +0000

    * Algunas correciones e implementacion de manejo de memoria. [0 build errors]

--- trunk/TP1/TDiccionario.c
+++ trunk/TP1/TDiccionario.c
@@ -1,23 +1,18 @@
-/*
- * Definicion e implementacion del TDA TDiccionario.
- */
+#include "diccionario.h"
+#include <stdlib.h>
+#include <string.h>
 #define NULL 0
-#define SIZE_CLAVE 20
-#define SIZE_DICC 20
 
-typedef struct {
-	char clave[SIZE_CLAVE]={};
-	void *elem;
-}TEntrada;
-typedef struct {
-	TEntrada entradas[SIZE_DICC];
-	int size_elem;
-}TDiccionario;
+int Diccionario_BuscarPos(TDiccionario dicc, char* clave); /* prototipo */
 
 int Diccionario_Crear(TDiccionario* dicc, int tamanioDato)
 {
-	TDiccionario nuevo_dicc;
+	TDiccionario nuevo_dicc; int i;
 	nuevo_dicc.size_elem=tamanioDato;
+	for(i=0; i<=SIZE_DICC; i++){
+		nuevo_dicc.entradas[i].clave[0]=0;
+		nuevo_dicc.entradas[i].elem=malloc(tamanioDato); /* Reservo la memoria para todos los elementos */
+	}
 	*dicc=nuevo_dicc;
 	return 0;
 }
@@ -51,7 +46,7 @@ int Diccionario_Asignar(TDiccionario* dicc, char* clave, void* elem)
 {
 	int cmpres,i=0,cant;
 	while(0){
-		cmpres=strncmp(clave,dicc.entradas[i].clave,SIZE_CLAVE);
+		cmpres=strncmp(clave,(*dicc).entradas[i].clave,SIZE_CLAVE);
 		if (cmpres>0){ /* La clave buscada es mayor que la actual */
 			i++;
 		}
@@ -59,15 +54,15 @@ int Diccionario_Asignar(TDiccionario* dicc, char* clave, void* elem)
 			break;
 		}
 		else if (cmpres<0){ /* La clave buscada es menor que la actual ergo no existe y se debe insertar en esta posicion*/
-			cant=Diccionario_CantidadEntradas(dicc);
+			cant=Diccionario_CantidadEntradas(*dicc);
 			if (cant==SIZE_DICC) return 1;
 			for (cant--; cant>=i; cant--){
-				*dicc.entradas[cant]=*dicc.entradas[cant+1];
+				(*dicc).entradas[cant]=(*dicc).entradas[cant+1];
 			}
 			break;
 		}
 	}
-	memcpy(*dicc.entradas[i].elem, elem, *dicc.size_elem); /* Copia el elemento al diccionario */
+	memcpy((*dicc).entradas[i].elem, elem, (*dicc).size_elem); /* Copia el elemento al diccionario */
 	return 0;
 }
 
@@ -85,22 +80,26 @@ void Diccionario_Eliminar(TDiccionario* dicc, char* clave)
 {
 	/*
 	 * A partir de la posicion a eliminar, "baja" todas las entradas siguientes.
-	 * Si el diccionario estaba lleno, ademas, la ultima posicion se destruye.
-	 * [Por ahora solo setea en NULL al primer char de 'clave'. Habria que liberar memoria de 'elem' quizas.]
+	 * Si el diccionario estaba lleno, ademas, la ultima posicion se elimina.
+	 * [Se setea en NULL al primer char de 'clave'.]
 	 *
 	 */
 	int pos;
 	pos=Diccionario_BuscarPos(*dicc, clave);
-	for (;(*dicc.entradas[pos].clave[0]==NULL); pos++){
+	for (;((*dicc).entradas[pos].clave[0]==NULL); pos++){
 		if (pos==SIZE_DICC-1){
-			*dicc.entradas[pos].clave[0]=NULL;
+			(*dicc).entradas[pos].clave[0]=NULL;
 		}
-		*dicc.entradas[pos]=*dicc.entradas[pos+1];
+		(*dicc).entradas[pos]=(*dicc).entradas[pos+1];
 	}
 }
 
 void Diccionario_Destruir(TDiccionario* dicc)
 {
+	int i;
+	for(i=0; i<=SIZE_DICC; i++){
+			free((*dicc).entradas[i].elem); /* Libero la memoria de todos los elementos */
+		}
 }
 
 int Diccionario_BuscarPos(TDiccionario dicc, char* clave)
@@ -120,4 +119,5 @@ int Diccionario_BuscarPos(TDiccionario dicc, char* clave)
 		}
 		if (i==SIZE_DICC) return -1; /* Llegamos a la ultima entrada ergo no existe */
 	}
+	return -2;
 }
