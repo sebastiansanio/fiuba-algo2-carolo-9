3f1fe0f6bd3c3da8b95450c35e3f67a4ee7edd5d
guillobuteler <guillobuteler@08861704-a7f1-11de-892d-7b2b76e7707e>
2009-09-29 08:47:24 +0000

    * Correcciones a todo.

--- TP1/TP1/menu.c
+++ TP1/TP1/menu.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "propiedades.h"
-#include <string.h> /* para que esta? */
 
 int menu ()
 {
     char op_menu_char[10];
     int op_menu_int;
-    TPropiedades propiedades; /* TPropiedades es el TDA que debidamente ser� creado*/
-    char nombre[SIZE_CLAVE];             /* Declar� todas la variables ac�. D�ganme si les parece bien, o si las deber�a declara en el Main.*/
+    TPropiedades propiedades;
+    char nombre[SIZE_CLAVE];
     char* nombres [15];
     char valor[SIZE_VALOR];
     char valorDefault[SIZE_VALOR];
@@ -16,11 +15,10 @@ int menu ()
     int validaCreacion; /* esta variable esta para que no pinche cuando destruimos una propiedad sin haberla creado antes*/
 
     validaCreacion=-1;
-    /* L�neas 11 a la 56 son el t�pico men� que hac�amos en Algo1. D�gname si hay alg� error.*/
 
     do
 		{
-        printf ("Seleccione la opcion deseada\n");
+        printf ("Seleccione la opcion deseada:\n");
 		printf ("1 Crear Propiedad\n");
 		printf ("2 Cargar Propiedad\n");
 		printf ("3 Guardar Propiedad\n");
@@ -29,86 +27,90 @@ int menu ()
 		printf ("6 Existe Propiedad\n");
 		printf ("7 Nombre Propiedades\n");
 		printf ("8 Eliminar Propiedad\n");
-		printf ("9 Destuir Propiedad\n");
-		printf ("10 Salir Programa\n");
+		printf ("9 Destruir Propiedad\n");
+		printf ("10 Salir\n");
 		scanf ("%s",op_menu_char);
 		op_menu_int=atoi(op_menu_char);
 		switch (op_menu_int)
 		{
 		case 1:
 		{
-		Propiedades_Crear (&propiedades); /* pasa con & porque yo lo declar� como variable y no como puntero a variable*/
-		validaCreacion=0;
-		printf ("La Propiedad ha sido creada satisfactoriamete\n");
-		break;
+			if (!Propiedades_Crear(&propiedades)){ /* '&' pues propiedades no es puntero.*/
+				validaCreacion=0;
+				printf ("La Propiedad ha sido creada satisfactoriamente.\n");
+			}
+			break;
 		}
 		case 2:
 		{
-		printf ("Ingrese el Path (i.e. la dirección del Archivo) de propiedades\n");
-		scanf ("%s",rutaArchivo);
-		Propiedades_Cargar(&propiedades,rutaArchivo); /* pasa con & porque yo lo declar� como variable y no como puntero a variable*/
-        printf ("La Propiedad ha sido cargada satisfactoriamete\n");
-		break;}
+			printf ("Ingrese la ruta al archivo de propiedades:\n");
+			scanf ("%s",rutaArchivo);
+			if (!Propiedades_Cargar(&propiedades,rutaArchivo)){
+				printf ("La Propiedad ha sido cargada satisfactoriamente.\n");
+			}
+			break;
+		}
 		case 3:
 		{
-		printf ("Ingrese el Path (i.e. la dirección del Archivo) de propiedades\n");
-		scanf ("%s",rutaArchivo);
-		Propiedades_Guardar(propiedades,rutaArchivo);
-        printf ("La Propiedad ha sido guardada satisfactoriamete\n");
-		break;
+			printf ("Ingrese la ruta al archivo de propiedades:\n");
+			scanf ("%s",rutaArchivo);
+			if (!Propiedades_Guardar(propiedades,rutaArchivo)){
+				printf ("La Propiedad ha sido guardada satisfactoriamete\n");
+			}
+			break;
 		}
 		case 4:
 		{
-        printf("Ingrese nombre de la propiedad\n");
-        scanf("%s",nombre);
-		Propiedades_Obtener(propiedades,nombre,valorDefault,valor); /* pasa con & porque yo lo declar� como variable y no como puntero a variable*/
-		printf (" El valor de la Propiedad es: %s\n",valor);
-		break;
+			printf("Ingrese nombre de la propiedad\n");
+			scanf("%s",nombre);
+			Propiedades_Obtener(propiedades,nombre,valorDefault,valor);
+			printf (" El valor de la Propiedad es: %s\n",valor);
+			break;
 		}
 		case 5:
 		{
-		printf("Ingrese nombre de la propiedad\n");
-        scanf("%s",nombre);
-        printf("Ingrese valor de la propiedad\n");
-        scanf("%s",valor);
-		Propiedades_Asignar(&propiedades,nombre,valor);
-
-		break;
+			printf("Ingrese el nombre de la propiedad:\n");
+			scanf("%s",nombre);
+			printf("Ingrese el valor de la propiedad:\n");
+			scanf("%s",valor);
+			if (!Propiedades_Asignar(&propiedades,nombre,valor)){
+				printf("Propiedad cargada satisfactoriamente.\n");
+			}
+			break;
 		}
 		case 6:
 		{
-		printf("Ingrese nombre de la propiedad\n");
-        scanf("%s",nombre);
-		if (Propiedades_Existe(propiedades,nombre)) {
-		    printf ("La Propiedad existe\n");}
-		    else{
-		        printf ("La propiedad NO existe\n");
-		        };
-		break;
+			printf("Ingrese el nombre de la propiedad:\n");
+			scanf("%s",nombre);
+			if (Propiedades_Existe(propiedades,nombre)) {
+				printf("La propiedad existe.\n");
+			}else{
+				printf ("La propiedad NO existe.\n");
+			}
+			break;
 		}
 		case 7:
 		{
-		Propiedades_Nombres(propiedades,&nombres[15]);
-		break;
+			Propiedades_Nombres(propiedades,nombres);
+			break;
 		}
 		case 8:
 		{
-		printf("Ingrese nombre de la propiedad\n");
-        scanf("%s",nombre);
-        Propiedades_Eliminar(&propiedades,nombre);
-        printf ("La Propiedad ha sido eliminada satisfactoriamete\n");
-		break;
+			printf("Ingrese el nombre de la propiedad:\n");
+			scanf("%s",nombre);
+			Propiedades_Eliminar(&propiedades,nombre);
+			printf("La Propiedad ha sido eliminada satisfactoriamente.\n");
+			break;
 		}
 		case 9:
 		{
-		    if (!(validaCreacion=-1)){
-		Propiedades_Destruir(&propiedades);
-        prinf ("La Propiedad ha sido destruida satisfactoriamete\n");
-		}
-		break;
+			if (validaCreacion==0){
+				Propiedades_Destruir(&propiedades);
+				printf("La Propiedad ha sido destruida satisfactoriamente.\n");
+			}
+			break;
 		}
 		} /* cierra el Switch*/
-	}
-	while (!(op_menu_int==10));
+	}while (op_menu_int!=10);
 	return 0;
 } /* cierra la funci�n menu*/
