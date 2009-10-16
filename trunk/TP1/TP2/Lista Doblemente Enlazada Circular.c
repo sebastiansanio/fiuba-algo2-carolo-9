58ac24fd50e1e28ae03ecf334bd488ef84840fb7
Lothcaramon <Lothcaramon@08861704-a7f1-11de-892d-7b2b76e7707e>
2009-10-16 22:13:21 +0000

    pequeñas modificaciones.
    aviso q no se bien por donde encarar el tema de la programación, asique ayudenmé un poco.

--- trunk/TP1/TP2/Lista Doblemente Enlazada Circular.c
+++ trunk/TP1/TP2/Lista Doblemente Enlazada Circular.c
@@ -7,6 +7,7 @@ int crear_Lista_DEC(TLista_DEC* listaDEC, f_clonar clonador, f_destruir destruct
 
 
 
+
 int destruir_Lista_DEC(TLista_DEC* listaDEC);
 
 int insertar_En_Lista_DEC(TLista_DEC* listaDEC, void* elemento, int posicion);
