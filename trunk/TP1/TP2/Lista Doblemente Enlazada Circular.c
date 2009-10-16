41eafd6c825bd5622f4387af273aee2cf0164bc0
Lothcaramon <Lothcaramon@08861704-a7f1-11de-892d-7b2b76e7707e>
2009-10-16 22:07:09 +0000

    Aca subo los archivos creados. Estan vacíos, asique si ya hicieron algo, copy pasteen

---
+++ trunk/TP1/TP2/Lista Doblemente Enlazada Circular.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Lista Doblemente Enlazada Circular.h"
+
+int crear_Lista_DEC(TLista_DEC* listaDEC, f_clonar clonador, f_destruir destructor)
+{
+
+
+
+int destruir_Lista_DEC(TLista_DEC* listaDEC);
+
+int insertar_En_Lista_DEC(TLista_DEC* listaDEC, void* elemento, int posicion);
+
+int obtener_Cte_Lista_DEC(TLista_DEC listaDEC, void** elemento);
+
+int mover_Cte_Lista_DEC(TLista_DEC* listaDEC, int posicion);
+
+int modificar_Cte_Lista_DEC(TLista_DEC* listaDEC, void* elemento);
+
+int vacia_Lista_DEC(TLista_DEC* listaDEC);
+
+int es_Primero_Lista_DEC(TLista_DEC* listaDEC);
---
+++ trunk/TP1/TP2/Lista de Reproducción.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Lista de Reproducción.h"
+
+
+int crear_Lista_Reproducción(TLista_Reproducción* listaReproducción, char* nomArch)
+{
+return 0;
+};
+
+int reproducir_Lista_Reproducción(Lista_Reproducción listaReproducción, int cantidad)
+{
+return 0;
+};
+
+int adelantar_Lista_Reproducción(Lista_Reproducción listaReproducción)
+{
+return 0;
+};
+
+int rotroceder_Lista_Reproducción(Lista_Reproducción listaReproducción)
+{
+return 0;
+};
+
+int ordenar_Lista_Reproducción(Lista_Reproducción* listaReproducción, char* orden)
+{
+return 0;
+};
+
+int guardar_Lista_Reproducción(Lista_Reproducción* listaReproducción, char* nomArch)
+{
+return 0;
+};
+
+int destruir_Lista_Reproducción(Lista_Reproducción* listaReproducción)
+{
+return 0;
+};
