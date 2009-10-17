a2a3b02a02d1a8779525964a476914c7a0b78ff4
seba90s <seba90s@08861704-a7f1-11de-892d-7b2b76e7707e>
2009-10-17 01:59:05 +0000

    Primitivas de crear y destruir lista.

--- TP1/TP2/Lista Doblemente Enlazada Circular.c
+++ TP1/TP2/Lista Doblemente Enlazada Circular.c
@@ -2,13 +2,38 @@
 #include <stdlib.h>
 #include "Lista Doblemente Enlazada Circular.h"
 
-int crear_Lista_DEC(TLista_DEC* listaDEC, f_clonar clonador, f_destruir destructor)
+/*PRE: listaDEC no creada */
+/*PRE: listaDEC creada */
+int crear_Lista_DEC(TLista_DEC* listaDEC, f_clonar clonador, f_destruir destructor, int TamanioDato)
 {
+    listaDec->TamanioDato=TamanioDato;
+    listaDEC->clonador=clonador;
+    listaDEC->destructor=destructor;
+    listaDEC->primero = NULL;
+    listaDEC->corriente = NULL;
+    return 0;
+    }
 
+/*PRE: listaDEC creada */
+/*PRE: listaDEC no creada */
+int destruir_Lista_DEC(TLista_DEC* listaDEC)
+{
+    	TNodoDoble *pAux;
+    	listaDEC->corriente=listaDEC->primero->siguiente;
+    	while (listaDEC->corriente!=listaDEC->primero)
+    	{
+    	    listaDEC->destructor(listaDEC->corriente->Elem);
+    	    pAux=listaDEC->corriente->siguiente;
+    	    free(listaDEC->corriente);
+    	    listaDEC->corriente=pAux;
+    	}
+            listaDEC->destructor(listaDEC->corriente->Elem);
+            free(listaDEC->corriente);
+            listaDEC->primero=listaDEC->corriente=NULL;
+            listaDEC->destructor=NULL;
+            listaDEC->clonador=NULL;
+    }
 
-
-
-int destruir_Lista_DEC(TLista_DEC* listaDEC);
 
 int insertar_En_Lista_DEC(TLista_DEC* listaDEC, void* elemento, int posicion);
 
@@ -21,3 +46,4 @@ int modificar_Cte_Lista_DEC(TLista_DEC* listaDEC, void* elemento);
 int vacia_Lista_DEC(TLista_DEC* listaDEC);
 
 int es_Primero_Lista_DEC(TLista_DEC* listaDEC);
+
--- TP1/TP2/Lista Doblemente Enlazada Circular.h
+++ TP1/TP2/Lista Doblemente Enlazada Circular.h
@@ -1,20 +1,26 @@
 #ifndef LISTA_DOBLEMENTE_ENLAZADA_CIRCULAR_H_INCLUDED
 #define LISTA_DOBLEMENTE_ENLAZADA_CIRCULAR_H_INCLUDED
 
+typedef void (*f_destruir)(void*);
+
+typedef void* (*f_clonar)(void*);
+
 typedef struct TNodo_Doble
 {
 void* Elem;
 struct TNodo_Doble *siguiente,*anterior;
-} TNodoNodo_Doble; /* Nodo de una lista doblemente enlazada */
+} TNodoDoble; /* Nodo de una lista doblemente enlazada */
 
 
 typedef struct
 {
 TNodoDoble *primero, *corriente;
 int TamanioDato;
+f_clonar clonador;
+f_destruir destructor;
 } TLista_DEC; /*Lista doblemente enlazada */
 
-int crear_Lista_DEC(TLista_DEC* listaDEC, f_clonar clonador, f_destruir destructor);
+int crear_Lista_DEC(TLista_DEC* listaDEC, f_clonar clonador, f_destruir destructor, int TamanioDato);
 
 int destruir_Lista_DEC(TLista_DEC* listaDEC);
 
