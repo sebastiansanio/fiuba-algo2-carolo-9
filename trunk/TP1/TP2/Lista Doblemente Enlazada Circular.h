e3a96851f7bffd076baf87fab4494ce1b06de1d4
seba90s <seba90s@08861704-a7f1-11de-892d-7b2b76e7707e>
2009-10-23 23:12:36 +0000

    Lista doblemente enlazada circular terminada y funcionando (compila).

--- trunk/TP1/TP2/Lista Doblemente Enlazada Circular.h
+++ trunk/TP1/TP2/Lista Doblemente Enlazada Circular.h
@@ -1,15 +1,15 @@
 #ifndef LISTA_DOBLEMENTE_ENLAZADA_CIRCULAR_H_INCLUDED
 #define LISTA_DOBLEMENTE_ENLAZADA_CIRCULAR_H_INCLUDED
 /*Codigos de error*/
-#define FALTA_MEMORIA=1;
-#define LISTA_DESTRUIDA=0;
-#define ELEMENTO_OBTENIDO=0;
-#define LISTA_VACIA=1;
-#define ELEMENTO_INSERTADO=0;
-#define LISTA_LLENA=1;
-#define LISTA_SIN_ELEMENTOS=0;
-#define ES_PRIMERO_VERDADERO=1;
-#define ES_PRIMERO_FALSO=0;
+#define FALTA_MEMORIA 1
+#define LISTA_DESTRUIDA 0
+#define ELEMENTO_OBTENIDO 0
+#define LISTA_VACIA 1
+#define ELEMENTO_INSERTADO 0
+#define LISTA_LLENA 1
+#define LISTA_SIN_ELEMENTOS 0
+#define ES_PRIMERO_VERDADERO 1
+#define ES_PRIMERO_FALSO 0
 
 typedef void (*f_destruir)(void*);
 
@@ -29,11 +29,12 @@ f_clonar clonador;
 f_destruir destructor;
 } TLista_DEC; /*Lista doblemente enlazada */
 
-enum Posicion
+enum posicion
 {
-    Anterior;
-    Primero;
-    Siguiente;};
+    Anterior,
+    Primero,
+    Siguiente
+    } ;
 
 int crear_Lista_DEC(TLista_DEC* listaDEC, f_clonar clonador, f_destruir destructor, int TamanioDato);
 
