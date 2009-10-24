8e4125f0d8d0aa06c4023e6e586422b44a010c2f
guillobuteler <guillobuteler@08861704-a7f1-11de-892d-7b2b76e7707e>
2009-10-24 01:04:06 +0000

    + clonar_Propiedades, destruir_Propiedades + crear_LR() [inconcluso]

---
+++ trunk/TP1/TP2/Lista de Reproduccion.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Lista de Reproduccion.h"
+#define RES_FOPEN_FAILED 2
+#define RES_OUT_OF_MEM 1
+
+void* clonar_Propiedades(void* propiedades);
+void destruir_Propiedades(void* propiedades);
+
+int crear_Lista_Reproduccion(TLista_Reproduccion* listaReproduccion, char* nomArch)
+{
+	FILE* arch_m3u;
+	f_destruir destructor;
+	f_clonar clonador;
+
+	destructor = destruir_Propiedades;
+	clonador = clonar_Propiedades;
+
+	arch_m3u = fopen(nomArch,"r");
+	if (!arch_m3u){return RES_FOPEN_FAILED;}
+
+	crear_Lista_DEC(&listaReproduccion->lista, clonador, destructor);
+
+	while (!feof(arch_m3u)){
+		char linea[255],rutaProp[255];
+		TPropiedades propiedad;
+		Propiedades_Crear(&propiedad);
+
+		fscanf(arch_m3u, "%[^\n]\n", &linea);
+		if(!sscanf(linea,"#%[^#\n]", rutaProp)){
+			rutaProp[0]='\0';
+		}
+		if (rutaProp[0]){
+			if(!Propiedades_Cargar(&propiedad, rutaProp)){
+				insertar_En_Lista_DEC(&listaReproduccion->lista,&propiedad,LDEC_POS_SIG); /*Insertar en la lista como siguiente.*/
+			}
+		}
+	}
+
+	return 0;
+};
+
+int reproducir_Lista_Reproduccion(TLista_Reproduccion listaReproduccion, int cantidad)
+{
+return 0;
+};
+
+int adelantar_Lista_Reproduccion(TLista_Reproduccion listaReproduccion)
+{
+return 0;
+};
+
+int retroceder_Lista_Reproduccion(TLista_Reproduccion listaReproduccion)
+{
+return 0;
+};
+
+int ordenar_Lista_Reproduccion(TLista_Reproduccion* listaReproduccion, char* orden)
+{
+return 0;
+};
+
+int guardar_Lista_Reproduccion(TLista_Reproduccion* listaReproduccion, char* nomArch)
+{
+return 0;
+};
+
+int destruir_Lista_Reproduccion(TLista_Reproduccion* listaReproduccion)
+{
+return 0;
+};
+
+void* clonar_Propiedades(void* propiedades){
+	char *nombres[SIZE_CLAVE], *valor;
+	int i,cant;
+	TPropiedades *prop_clon = (TPropiedades*) malloc(sizeof(TPropiedades));
+	TPropiedades *prop_orig = (TPropiedades*)propiedades;
+	if (!prop_clon){return NULL;}
+
+	Propiedades_Crear(prop_clon);
+	cant=Propiedades_Nombres(*prop_clon,nombres);
+	for (i=0;i<cant;i++){
+		Propiedades_Obtener(*prop_orig,nombres[i],NULL,valor);
+		Propiedades_Asignar(prop_clon,nombres[i],valor);
+	}
+
+	return prop_clon;
+}
+
+void destruir_Propiedades(void* propiedades){
+	propiedades = (TPropiedades*)propiedades;
+	Propiedades_Destruir(propiedades);
+}
---
+++ trunk/TP1/TP2/Lista de Reproduccion.h
@@ -0,0 +1,29 @@
+#ifndef LISTA_DE_REPRODUCCION_H_INCLUDED
+#define LISTA_DE_REPRODUCCION_H_INCLUDED
+#include "Lista Doblemente Enlazada Circular.h"
+#include "propiedades.h"
+#include "diccionario.h"
+
+typedef struct
+{
+TLista_DEC lista;
+TDiccionario dicc_alias;
+} TLista_Reproduccion; /* Lista de reproduccion */
+
+int crear_Lista_Reproduccion(TLista_Reproduccion* listaReproduccion, char* nomArch);
+
+int reproducir_Lista_Reproduccion(TLista_Reproduccion listaReproduccion, int cantidad);
+
+int adelantar_Lista_Reproduccion(TLista_Reproduccion listaReproduccion);
+
+int retroceder_Lista_Reproduccion(TLista_Reproduccion listaReproduccion);
+
+int ordenar_Lista_Reproduccion(TLista_Reproduccion* listaReproduccion, char* orden);
+
+int guardar_Lista_Reproduccion(TLista_Reproduccion* listaReproduccion, char* nomArch);
+
+int destruir_Lista_Reproduccion(TLista_Reproduccion* listaReproduccion);
+
+
+
+#endif // LISTA_DE_REPRODUCCION_H_INCLUDED
