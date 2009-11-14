b00a5da8b3b5d8ad67c8cf8d400a9aad9aa9d37e
seba90s <seba90s@08861704-a7f1-11de-892d-7b2b76e7707e>
2009-11-14 23:51:02 +0000

    Arbol binario

--- TP3/Arbol.h
+++ TP3/Arbol.h
@@ -1,6 +1,36 @@
 #ifndef ARBOL_H_INCLUDED
 #define ARBOL_H_INCLUDED
 
+#include <stdlib.h>
+#define IZQ 1
+#define DER 2
+#define PAD 3
+#define RAIZ 4
 
+typedef struct TNodoAB
+{
+void* elem;
+struct TNodoAB *izq, *der;
+} TNodoAB;
+
+typedef struct TAB
+{
+TNodoAB *raiz,*cte;
+int tamdato;
+} TAB;
+
+void AB_Crear(TAB *a,int tdato);
+
+void AB_ElemCte(TAB a,void *elem);
+
+void AB_ModifCte(TAB *a,void *elem);
+
+void AB_MoverCte(TAB *a,int mov,int *error);
+
+void AB_Vaciar(TAB *a);
+
+int AB_Vacio(TAB a);
+
+void AB_Insertar(TAB *a,int mov,void *elem,int *error);
 
 #endif
