a3f71c0a5584a0a11f99d1b9768ec309f392626e
seba90s <seba90s@08861704-a7f1-11de-892d-7b2b76e7707e>
2009-11-14 21:12:48 +0000

    Archivos del TP3

---
+++ TP3/Lista Doblemente Enlazada Circular.c
@@ -0,0 +1,176 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Lista Doblemente Enlazada Circular.h"
+
+
+/*PRE: listaDEC no creada */
+/*POST: listaDEC creada */
+int crear_Lista_DEC(TLista_DEC* listaDEC, f_clonar clonador, f_destruir destructor)
+{
+    listaDEC->clonador=clonador;
+    listaDEC->destructor=destructor;
+    listaDEC->primero = NULL;
+    listaDEC->corriente = NULL;
+    return 0;
+}
+
+/*PRE: listaDEC creada */
+/*POST: listaDEC no creada */
+int destruir_Lista_DEC(TLista_DEC* listaDEC)
+{
+    TNodoDoble *pAux=(TNodoDoble*)malloc(sizeof(TNodoDoble));
+    if(!listaDEC->primero){return LISTA_VACIA;}
+    listaDEC->corriente=listaDEC->primero->siguiente;
+    while (listaDEC->corriente!=listaDEC->primero)
+    {
+        listaDEC->destructor(listaDEC->corriente->Elem);
+        pAux=listaDEC->corriente->siguiente;
+        free(listaDEC->corriente);
+        listaDEC->corriente=pAux;
+    }
+    listaDEC->destructor(listaDEC->corriente->Elem);
+    free(listaDEC->corriente);
+    listaDEC->primero=listaDEC->corriente=NULL;
+    listaDEC->destructor=NULL;
+    listaDEC->clonador=NULL;
+    return LISTA_DESTRUIDA;
+}
+
+
+/*PRE: listaDEC creada */
+/*POST: Si la lista est� vac�a devuelve LISTA_VACIA, sino elimina el actual elemento corriente y el corriente pasa a ser el siguiente */
+int eliminar_Cte_Lista_DEC(TLista_DEC* listaDEC)
+{
+    TNodoDoble *pAux;
+    if(!listaDEC->corriente)
+        return LISTA_VACIA;
+    else
+        if(listaDEC->corriente->siguiente!=listaDEC->corriente)
+            {
+            listaDEC->destructor(listaDEC->corriente->Elem);
+            listaDEC->corriente->anterior->siguiente=listaDEC->corriente->siguiente;
+            listaDEC->corriente->siguiente->anterior=listaDEC->corriente->anterior;
+            pAux=listaDEC->corriente->siguiente;
+            free(listaDEC->corriente);
+            if(listaDEC->corriente==listaDEC->primero)
+                listaDEC->corriente=listaDEC->primero=pAux;
+            else
+                listaDEC->corriente=pAux;
+            return 0;
+            }
+        else
+            {
+            listaDEC->destructor(listaDEC->corriente->Elem);
+            free(listaDEC->corriente);
+            listaDEC->primero=listaDEC->corriente=NULL;
+            return 0;
+            }
+
+}
+
+
+/*PRE: listaDEC creada */
+/*POST: Si la lista est� vac�a devuelve 1, sino guarda en elemento una copia del dato en corriente */
+int obtener_Cte_Lista_DEC(TLista_DEC listaDEC, void** elemento)
+{
+    if(!listaDEC.corriente)
+        return LISTA_VACIA;
+    else
+        {
+            *elemento=listaDEC.clonador(listaDEC.corriente->Elem);
+            return ELEMENTO_OBTENIDO;
+        }
+}
+
+
+/*PRE: listaDEC creada */
+/*POST: Si la lista est� vac�a devuelve LISTA_VACIA. Si posicion es 0 el corriente pasa al primero, sino el corriente se mueve la cantidad
+en posicion (hacia izquierda o derecha seg�n el signo)  */
+int mover_Cte_Lista_DEC(TLista_DEC* listaDEC, int posicion)
+{
+    if (!listaDEC->primero)
+        return LISTA_VACIA;
+    if (posicion==LDEC_POS_PRI)
+        {
+        listaDEC->corriente=listaDEC->primero;
+        return 0;
+        }
+    if (posicion==LDEC_POS_SIG)
+        listaDEC->corriente=listaDEC->corriente->siguiente;
+    else
+        listaDEC->corriente=listaDEC->corriente->anterior;
+    return 0;
+}
+
+/*PRE: listaDEC creada */
+/*POST: Si la lista est� vac�a devuelve LISTA_VACIA, sino guarda en corriente elemento */
+int modificar_Cte_Lista_DEC(TLista_DEC* listaDEC, void* elemento)
+{
+    if(!listaDEC->corriente)
+        return LISTA_VACIA;
+    else
+        {
+            listaDEC->destructor(listaDEC->corriente->Elem);
+            listaDEC->corriente->Elem=listaDEC->clonador(elemento);
+            return 0;
+        }
+}
+
+/*PRE: listaDEC creada */
+/*POST: Si la lista est� vac�a devuelve 1, sino devuelve 0 */
+int vacia_Lista_DEC(TLista_DEC* listaDEC)
+{
+    if(listaDEC->primero)
+        return LISTA_LLENA;
+    else
+        return LISTA_SIN_ELEMENTOS;
+}
+
+/*PRE: listaDEC creada y no vac�a */
+/*POST: Si el corriente es el primer elemento de la lista devuelve 1, sino devuelve 0. */
+int es_Primero_Lista_DEC(TLista_DEC* listaDEC)
+{
+    if(listaDEC->primero==listaDEC->corriente)
+        return ES_PRIMERO_VERDADERO;
+    else
+        return ES_PRIMERO_FALSO;
+}
+
+/*PRE: ldec creada, elemento y posici�n validos*/
+/*POST: Inserta seg�n posici�n el elemento en la lista, devuelve FALTA_MEMORIA si no pudo insertar, y ELEMENTO_INSERTADO si pudo*/
+int insertar_En_Lista_DEC(TLista_DEC* ldec, void* elemento, int posicion)
+{
+    TNodoDoble* pNodo=(TNodoDoble*)malloc(sizeof(TNodoDoble));
+    if(!pNodo){return FALTA_MEMORIA;}
+    pNodo->Elem=ldec->clonador(elemento);
+    if(!pNodo->Elem){free(pNodo);return FALTA_MEMORIA;}
+
+    if (ldec->primero == NULL){
+    	pNodo->anterior=pNodo->siguiente= pNodo;
+    	ldec->primero=ldec->corriente= pNodo;
+    	return ELEMENTO_INSERTADO;
+    }
+    if ((posicion==LDEC_POS_PRI) ||
+    ((posicion==LDEC_POS_ANT) && (ldec->primero==ldec->corriente))){
+        pNodo->siguiente=ldec->primero;
+        pNodo->anterior=ldec->primero->anterior;
+        ldec->primero->anterior->siguiente=pNodo;
+        ldec->primero->anterior=pNodo;
+        ldec->primero=ldec->corriente=pNodo;}
+    else{
+        if(posicion==LDEC_POS_SIG){
+            pNodo->siguiente=ldec->corriente->siguiente;
+            pNodo->anterior=ldec->corriente;
+            ldec->corriente->siguiente->anterior=pNodo;
+            ldec->corriente->siguiente=pNodo;
+            ldec->corriente=pNodo;}
+        else{/*Anterior*/
+            pNodo->siguiente=ldec->corriente;
+            pNodo->anterior=ldec->corriente->anterior;
+            ldec->corriente->anterior->siguiente=pNodo;
+            ldec->corriente->anterior=pNodo;
+            ldec->corriente=pNodo;}}
+    return ELEMENTO_INSERTADO;
+}
+
+
---
+++ TP3/Lista Doblemente Enlazada Circular.h
@@ -0,0 +1,57 @@
+#ifndef LISTA_DOBLEMENTE_ENLAZADA_CIRCULAR_H_INCLUDED
+#define LISTA_DOBLEMENTE_ENLAZADA_CIRCULAR_H_INCLUDED
+/*Codigos de error*/
+#define FALTA_MEMORIA 1
+#define LISTA_DESTRUIDA 0
+#define ELEMENTO_OBTENIDO 0
+#define LISTA_VACIA 1
+#define ELEMENTO_INSERTADO 0
+#define LISTA_LLENA 0
+#define LISTA_SIN_ELEMENTOS 1
+#define ES_PRIMERO_VERDADERO 1
+#define ES_PRIMERO_FALSO 0
+
+typedef void (*f_destruir)(void*);
+
+typedef void* (*f_clonar)(void*);
+
+typedef struct TNodo_Doble
+{
+void* Elem;
+struct TNodo_Doble *siguiente,*anterior;
+} TNodoDoble; /* Nodo de una lista doblemente enlazada */
+
+
+typedef struct
+{
+TNodoDoble *primero, *corriente;
+f_clonar clonador;
+f_destruir destructor;
+} TLista_DEC; /*Lista doblemente enlazada */
+
+enum posicion
+{
+    LDEC_POS_PRI,
+    LDEC_POS_SIG,
+    LDEC_POS_ANT
+    } ;
+
+int crear_Lista_DEC(TLista_DEC* listaDEC, f_clonar clonador, f_destruir destructor);
+
+int destruir_Lista_DEC(TLista_DEC* listaDEC);
+
+int insertar_En_Lista_DEC(TLista_DEC* listaDEC, void* elemento, int posicion);
+
+int obtener_Cte_Lista_DEC(TLista_DEC listaDEC, void** elemento);
+
+int eliminar_Cte_Lista_DEC(TLista_DEC* listaDEC);
+
+int mover_Cte_Lista_DEC(TLista_DEC* listaDEC, int posicion);
+
+int modificar_Cte_Lista_DEC(TLista_DEC* listaDEC, void* elemento);
+
+int vacia_Lista_DEC(TLista_DEC* listaDEC);
+
+int es_Primero_Lista_DEC(TLista_DEC* listaDEC);
+
+#endif
