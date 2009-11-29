7503ea06f18e811e0f88f910cec729cc85e8e440
seba90s <seba90s@08861704-a7f1-11de-892d-7b2b76e7707e>
2009-11-29 00:22:29 +0000

    AB_Busq_Destruir

--- trunk/TP3/arbol de busqueda.h
+++ trunk/TP3/arbol de busqueda.h
@@ -1,6 +1,8 @@
 #ifndef ARBOL_DE_BUSQUEDA_H_INCLUDED
 #define ARBOL_DE_BUSQUEDA_H_INCLUDED
 
+typedef int (*f_comparar)(void*,void*);
+
 typedef struct TNodoAB_BUSQ
 {
     void* elem;
@@ -11,14 +13,15 @@ typedef struct TAB
 {
     TNodoAB_BUSQ *raiz,*cte;
     int tamdato;
+    f_comparar comparar;
 } TAB_BUSQ;
 
-int (*comparar)(void*,void*);
-
-void AB_Busq_Crear(TAB_BUSQ*a,int tamdato);
+void AB_Busq_Crear(TAB_BUSQ*a,int tamdato,f_comparar comparar);
 
 int AB_Busq_Buscar(TAB_BUSQ*a,void*elem_a_comparar,void*elem_devuelto);
 
 int AB_Busq_Insertar(TAB_BUSQ*a,void*elem);
+
+void AB_Busq_Destruir(TAB_BUSQ*a);
 
 #endif
