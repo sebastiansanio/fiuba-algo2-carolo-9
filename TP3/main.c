68fd4540d108612a11032a3ab6a57d2119700748
seba90s <seba90s@08861704-a7f1-11de-892d-7b2b76e7707e>
2009-12-06 16:19:38 +0000

    Base de main.c

--- TP3/main.c
+++ TP3/main.c
@@ -2,8 +2,37 @@
 #include <stdlib.h>
 #include "Pantalla.h"
 #include "agenda.h"
+#include "ListaSimple.h"
 
-int main()
+int main(int argc,char*argv[])
 {
-    return 0;
+    char op_menu_char[10];
+    int op_menu_int;
+    char nomArch[255];
+    TDivision division;
+    TAgenda agenda;
+    TPantalla pantalla;
+    FILE archivo;
+    TListaSimple lista;
+    printf("Inserte ruta del archivo para crear pantalla: \n");
+    scanf("%s",nomArch);
+    fopen(archivo,nomArch);
+    LS_Crear(%lista);
+    while (!feof(archivo))
+    {
+        fscanf("%d,%d;%d,%d",division.inicio.x,division.inicio.y,division.fin.x,division.fin.y);
+        LS_Insertar(&lista,LS_SIGUIENTE,&division);
+    }
+    do
+		{
+        printf ("\nSeleccione la opcion deseada:\n");
+		printf ("10 Salir\n");
+		scanf ("%s",op_menu_char);
+		op_menu_int=atoi(op_menu_char);
+		switch (op_menu_int)
+		{
+		default: printf("Opcion incorrecta\n\n");
+        } /* cierra el Switch*/
+	}while (op_menu_int!=10);
+	return 0;
 }
