eb50541b62640f1e83fd1ef3837755d05e95f5e7
seba90s <seba90s@08861704-a7f1-11de-892d-7b2b76e7707e>
2009-12-12 17:34:01 +0000

    main terminado

--- trunk/TP3/aplicacion.c
+++ trunk/TP3/aplicacion.c
@@ -1,9 +1,52 @@
+#include "pantalla.h"
 #include "agenda.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include "aplicacion.h"
 
-void imprimir_por_pantalla(char*mensaje)
+
+void imprimir_por_pantalla(void*arg)
 {
-    printf("%s",mensaje);
+    printf("\n%s \n",(char*)arg);
 }
 
+void agregar_numero(void*arg)
+{
+    char nombre[255];
+    char telefono[255];
+    printf("Ingrese datos del contacto\n");
+    printf("Nombre:\n");
+    scanf("%s",nombre);
+    printf("Telefono:\n");
+    scanf("%s",telefono);
+    agenda_agregar((TAgenda*)arg,nombre,telefono);
+}
+
+void buscar_numero(void*arg)
+{
+    char nombre[255];
+    char telefono[255];
+    int i;
+    printf("Ingrese nombre del contacto a buscar \n");
+    scanf("%s",nombre);
+    i=agenda_buscar(*(TAgenda*)arg,nombre,telefono);
+    if (!i)
+        printf("El numero del contacto es %s\n",telefono);
+    else
+        printf("Contacto no existe");
+}
+
+void eliminar_numero(void*arg)
+{
+    char nombre[255];
+    printf("Ingrese nombre del contacto a eliminar\n");
+    scanf("%s",nombre);
+    agenda_borrar((TAgenda*)arg,nombre);
+}
+
+void exit_f(void*arg)
+{
+    agenda_destruir(((TExit*)arg)->agenda);
+    Pantalla_Destruir(((TExit*)arg)->pantalla);
+    exit(((TExit*)arg)->codigo_salida);
+}
--- trunk/TP3/main.c
+++ trunk/TP3/main.c
@@ -42,10 +42,12 @@ int main(int argc,char*argv[])
     }
     fclose(arch);
     Pantalla_Crear(&pantalla,&lista,sizeof(TSector));
+    ls_Vaciar(&lista);
     do
 		{
         printf ("\nSeleccione la opcion deseada:\n");
         printf ("1 Asociar accion \n");
+        printf("2 Ejecutar accion \n");
 		printf ("10 Salir\n");
 		scanf ("%s",op_menu_char);
 		op_menu_int=atoi(op_menu_char);
@@ -69,29 +71,83 @@ int main(int argc,char*argv[])
 		        {
 		        printf("Que accion desea asociar? \n");
 		        printf("1 Imprimir mensaje por pantalla \n");
+		        printf("2 Agregar contacto a la agenda\n");
+		        printf("3 Buscar contacto\n");
+		        printf("4 Eliminar contacto\n");
+		        printf("5 Salir\n");
 		        scanf("%s",opaux_char);
 		        opaux_int=atoi(opaux_char);
-		        if (opaux_int!=1)
+		        if (opaux_int!=1 && opaux_int!=2 && opaux_int!=3 && opaux_int!=4 && opaux_int!=5)
                     printf("Opcion incorrecta \n \n");
 		        }
-		        while (opaux_int!=1);
+		        while (opaux_int!=1 && opaux_int!=2 && opaux_int!=3 && opaux_int!=4 && opaux_int!=5);
 		        switch (opaux_int)
 		        {
 		        case 1:
                     {
-		            char mensaje[255];
-		            printf("Ingrese mensaje \n");
-		            scanf("%s",mensaje);
-		            sector.funcion=&printf;
-		            sector.arg=mensaje;
-		            Pantalla_Asociar_Elemento(&pantalla,punto,&sector);
-		            break;
+                        char mensaje[255];
+                        printf("Ingrese mensaje \n");
+                        scanf("%s",mensaje);
+                        sector.funcion=&imprimir_por_pantalla;
+                        sector.arg=mensaje;
+                        Pantalla_Asociar_Elemento(&pantalla,punto,&sector);
+                        break;
+                    }
+                case 2:
+                    {
+                        sector.funcion=&agregar_numero;
+                        sector.arg=&agenda;
+                        Pantalla_Asociar_Elemento(&pantalla,punto,&sector);
+                        break;
+                    }
+                case 3:
+                    {
+                        sector.funcion=&buscar_numero;
+                        sector.arg=&agenda;
+                        Pantalla_Asociar_Elemento(&pantalla,punto,&sector);
+                        break;
+                    }
+                case 4:
+                    {
+                        sector.funcion=&eliminar_numero;
+                        sector.arg=&agenda;
+                        Pantalla_Asociar_Elemento(&pantalla,punto,&sector);
+                        break;
+                    }
+                case 5:
+                    {
+                        TExit aux_exit;
+                        aux_exit.codigo_salida=0;
+                        aux_exit.agenda=&agenda;
+                        aux_exit.pantalla=&pantalla;
+                        sector.funcion=&exit_f;
+                        sector.arg=&aux_exit;
+                        Pantalla_Asociar_Elemento(&pantalla,punto,&sector);
+                        break;
                     }
 		        }
-
-
 		        break;
 		    }
+		    case 2:
+		    {
+		        TPunto punto;
+		        double aux_x,aux_y;
+		        int error;
+		        do
+		        {
+		            printf("\nIngrese coordenadas con formato x:y (entre 0 y 1)\n \n");
+                    scanf("%lf:%lf",&aux_x,&aux_y);
+                    if (aux_x<0 || aux_x>1 || aux_y<0 || aux_y>1)
+                        printf("\nCoordenadas incorrectas \n");
+		        } while (aux_x<0 || aux_x>1 || aux_y<0 || aux_y>1);
+		        Punto_Cargar(&punto,aux_x,aux_y);
+		        error=Pantalla_Obtener_Elemento(&pantalla,punto,&sector);
+		        if (error==TPAN_OK)
+                    sector.funcion(sector.arg);
+                else
+                    printf("\nNo hay accion asociada al sector \n");
+		    }
+
 		    case 10:
 		    {
 		        break;
@@ -99,7 +155,6 @@ int main(int argc,char*argv[])
             default: printf("Opcion incorrecta\n\n");
         } /* cierra el Switch*/
 	}while (op_menu_int!=10);
-	ls_Vaciar(&lista);
 	Pantalla_Destruir(&pantalla);
 	agenda_destruir(&agenda);
 	return 0;
